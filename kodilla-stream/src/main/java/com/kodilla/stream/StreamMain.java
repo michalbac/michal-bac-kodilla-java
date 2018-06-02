4460143a20145c6077d2938e4680e7f7d569c2e5
“michalbac <michal.bac1993@gmail.com>
2018-06-02 11:44:57 +0200

    Stream-ForumUser

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,6 +3,7 @@ package com.kodilla.stream;
 
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 
 import static jdk.nashorn.internal.objects.NativeString.toLowerCase;
@@ -17,5 +18,8 @@ public class StreamMain {
         poemBeautifier.beautify("Poem2", text -> toUpperCase (text));
         poemBeautifier.beautify("Poem3", text -> "@" + text + "@");
         poemBeautifier.beautify("Poem4", text -> toLowerCase (text));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
