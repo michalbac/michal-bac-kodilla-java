bbe8157c9b1fd458061073f208af88173638e9cd
“michalbac <michal.bac1993@gmail.com>
2018-06-13 09:53:36 +0200

    9.1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -1,39 +1,40 @@
-//package com.kodilla.good.patterns.challenges;
-//
-//import java.util.*;
-//import java.util.stream.Collectors;
-//
-//
-//public final class MovieStore {
-//
-//    public Map<String, List<String>> getMovies() {
-//
-//        List<String> ironManTranslations = new ArrayList<>();
-//        ironManTranslations.add("Żelazny Człowiek");
-//        ironManTranslations.add("Iron Man");
-//
-//        List<String> avengersTranslations = new ArrayList<>();
-//        avengersTranslations.add("Mściciele");
-//        avengersTranslations.add("Avengers");
-//
-//        List<String> flashTranslations = new ArrayList<>();
-//        flashTranslations.add("Błyskawica");
-//        flashTranslations.add("Flash");
-//
-//        Map<String, List<String>> booksTitlesWithTranslations = new HashMap<>();
-//        booksTitlesWithTranslations.put("IM", ironManTranslations);
-//        booksTitlesWithTranslations.put("AV", avengersTranslations);
-//        booksTitlesWithTranslations.put("FL", flashTranslations);
-//
-//        return booksTitlesWithTranslations;
-//    }
-//
-//    public List<String> getTitles() {
-//        List<String> titles = getMovies().stream()
-//                .flatMap(title -> title.stream())
-//                .map()
-//                .collect(Collectors.toList());
-//
-//    }
-//
-//}
+package com.kodilla.good.patterns.challenges;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public final class MovieStore {
+
+    public Map<String, List<String>> getMovies() {
+
+        List<String> ironManTranslations = new ArrayList<>();
+        ironManTranslations.add("Żelazny Człowiek");
+        ironManTranslations.add("Iron Man");
+
+        List<String> avengersTranslations = new ArrayList<>();
+        avengersTranslations.add("Mściciele");
+        avengersTranslations.add("Avengers");
+
+        List<String> flashTranslations = new ArrayList<>();
+        flashTranslations.add("Błyskawica");
+        flashTranslations.add("Flash");
+
+        Map<String, List<String>> booksTitlesWithTranslations = new HashMap<>();
+        booksTitlesWithTranslations.put("IM", ironManTranslations);
+        booksTitlesWithTranslations.put("AV", avengersTranslations);
+        booksTitlesWithTranslations.put("FL", flashTranslations);
+
+        return booksTitlesWithTranslations;
+    }
+
+    public String getTitles() {
+        String titles = getMovies().values().stream()
+                .flatMap(title -> title.stream())
+                .collect(Collectors.joining("! "));
+
+        return titles;
+
+    }
+
+}
